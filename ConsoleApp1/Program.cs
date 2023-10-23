21d93e6ec0b71a829c376ae217d75d9d84f5ba04
fmartinez <fmartinez@baufest.com>
2023-10-23 20:10:44 -0300

    subo cambios

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+
+var c = new Shared();
+
+int num = c.DevolverNum();
+
+Console.WriteLine("res" +  num);
---
+++ ConsoleApp1/Shared.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Shared
+    {
+        public int[] Numeros;
+        public Shared()
+        {
+            this.Numeros = new int[6];
+            this.Numeros[0] = 1;
+            this.Numeros[1] = 2;
+            this.Numeros[2] = 3;
+            this.Numeros[3] = 4;
+        }
+
+       
+        public int DevolverNum()
+        {
+            return this.Numeros.Where(x => x == 5).FirstOrDefault();
+        }
+    }
+}
